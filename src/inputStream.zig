9a3fee8db93a44f9326274f0edcd26272bf06889
Joseph Bronsten <josephbronsten@gmail.com>
2025-09-20 21:13:55 -0700

    Fix obvious syntax error

--- src/inputStream.zig
+++ src/inputStream.zig
@@ -5,48 +5,48 @@ pub const InputStream = struct {
     data: []const u8,
     pos: usize = 0,
 
-    pub fn init(data: []const u8) InputStream{
-        return .{.data = data};
+    pub fn init(data: []const u8) InputStream {
+        return .{ .data = data };
     }
 
     pub fn consumeChar(stream: *InputStream) ?u8 {
-        if(stream.pos+1 >= stream.data.size){ return null;}
+        if (stream.pos + 1 >= stream.data.len) {
+            return null;
+        }
         stream.pos += 1;
         return stream.data[stream.pos];
     }
-    pub fn reconsumeChar(stream: *InputStream) void{
+
+    pub fn reconsumeChar(stream: *InputStream) void {
         stream.pos -= 1;
     }
-    
+
     pub fn peek(stream: *InputStream, offset: usize) ?u8 {
-        if (stream.pos + offset >= stream.data.size) {
+        if (stream.pos + offset >= stream.data.len) {
             return null;
         }
         return stream.data[stream.pos + offset];
     }
-    
-    pub fn nextCharsAre(stream: *InputStream, string: []const u8) bool{
-        for (string, 0.. ) |checkChar, i| {
+
+    pub fn nextCharsAre(stream: *InputStream, string: []const u8) bool {
+        for (string, 0..) |checkChar, i| {
             const maybeChar: ?u8 = stream.peek(i);
             if (maybeChar) |char| {
                 if (char != checkChar) {
-                    return false; 
+                    return false;
                 }
             } else {
                 return false;
             }
-
         }
         return true;
     }
 
-    pub fn consumeString(stream: *InputStream, string: []const u8) bool{
-        if(stream.nextCharsAre(string)) {
+    pub fn consumeString(stream: *InputStream, string: []const u8) bool {
+        if (stream.nextCharsAre(string)) {
             stream.pos += string.len;
             return true;
         }
         return false;
     }
-    
-     
 };
