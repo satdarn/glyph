1a8d86bb499a881cb612256a31c401478c7752d9
Joseph Bronsten <josephbronsten@gmail.com>
2025-10-27 16:46:47 -0700

    Start Parsing

--- src/parser.zig
+++ src/parser.zig
@@ -2,14 +2,17 @@ const std = @import("std");
 const HtmlLexer = @import("lexer.zig").HtmlLexer;
 const Tokens = @import("tokens.zig").Tokens;
 const Nodes = @import("nodes.zig").Nodes;
+const InputStream = @import("inputStream.zig").InputStream;
 
 pub const HtmlParser = struct {
     lexer: HtmlLexer,
     allocator: std.mem.Allocator,
-    pub fn init(allocator: std.mem.Allocator, lexer: HtmlLexer) !HtmlParser {
-        return .{.lexer=lexer, .allocator=allocator};
+    pub fn init(allocator: std.mem.Allocator) !HtmlParser {
+        const stream = InputStream.init("<!DOCTYPE html> \n <html> <head><meta/></head><body></body></html> \n");
+        const lexer = try HtmlLexer.init(allocator, stream);
+        return .{ .lexer = lexer, .allocator = allocator };
     }
-    pub fn deinit(self :*HtmlParser) void{
-        _ = self.lexer.tokenHandler.getQueueLen();
+    pub fn deinit(self: *HtmlParser) void {
+        self.lexer.deinit();
     }
 };
