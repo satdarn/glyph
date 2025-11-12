8a03f44acf89d71baf384225873a78b2cc93b307
Joseph Bronsten <josephbronsten@gmail.com>
2025-11-12 12:39:36 -0800

    Initial insertion mode :)

--- src/parser.zig
+++ src/parser.zig
@@ -32,7 +32,7 @@ pub const HtmlParser = struct {
     lexer: HtmlLexer,
     allocator: std.mem.Allocator,
     pub fn init(allocator: std.mem.Allocator) !HtmlParser {
-        const stream = InputStream.init("<!DOCTYPE html> \n <html> <head><meta/></head><body></body></html> \n");
+        const stream = InputStream.init("<!DOCTYPE HtmL> \n <html> <head><meta/></head><body></body></html> \n");
         const lexer = try HtmlLexer.init(allocator, stream);
         return .{ .lexer = lexer, .allocator = allocator };
     }
@@ -40,11 +40,21 @@ pub const HtmlParser = struct {
         self.lexer.deinit();
     }
     pub fn getTree(self: *HtmlParser) !void {
+        self.lexer.verbose = true;
         const insertion_mode: InsertionMode = .Initial;
         const token: *Token = try self.lexer.nextToken();
-        // const document: *Node = try Node.createDocument(self.allocator); 
+        const document: *Node = try Node.createDocument(self.allocator);
+        defer document.deinit();
         switch (insertion_mode) {
             .Initial => {
+                if (token.* == Token.Comment) {
+                    try document.children.append(self.allocator, try Node.createComment(self.allocator, token.Comment.data.items));
+                }
+                if (token.* == Token.DOCTYPE) {
+                    if (std.mem.eql(u8, token.DOCTYPE.name.items, "html")) {
+                        try document.children.append(self.allocator, try Node.createDOCTYPE(self.allocator, token.DOCTYPE.name.items, null, null, false));
+                    }
+                }
             },
             .BeforeHtml => {},
             .BeforeHead => {},
